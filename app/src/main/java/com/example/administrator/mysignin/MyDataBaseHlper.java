b03ead1c4eaeb455d1d96fcb27f787d7df7ad060
NewSoulTechonogy <5406120102qq.com>
2016-11-05 12:00:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/mysignin/MyDataBaseHlper.java
@@ -0,0 +1,30 @@
+package com.example.administrator.mysignin;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Administrator on 2016/10/31.
+ */
+public class MyDataBaseHlper extends SQLiteOpenHelper {
+
+    public MyDataBaseHlper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table if not exists student_info(student_id text not null,student_name text not null," +
+                "student_class text not null)");
+        sqLiteDatabase.execSQL("create table if not exists student_usual_score(student_id text ,student_qimhjia integer  null,student_chidao integer  null," +
+                "student_kuangke integer  null,student_zaotui integer  null,student_chuqin integer  null)");
+        sqLiteDatabase.execSQL("create table if not exists student_lab_score(student_id integer ,student_score integer  null)");
+        sqLiteDatabase.execSQL("create table if not exists student_exam_score(student_id text ,student_score integer  null)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
